4162ed239b03a0dc4903d31410411810dc5e1e13
nanimopopo <am_nurzhanov@kbtu.kz>
2026-04-05 15:48:07 +0300

    Lab9

---
+++ Lab 9/shop-back/api/serializers.py
@@ -0,0 +1,11 @@
+from rest_framework import serializers
+from api.models import *
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
